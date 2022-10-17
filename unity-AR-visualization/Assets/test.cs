9daa40867c6f17049bb20c10ec1b1efe71cc2099
unknown <colemanliyah@gmail.com>
2022-10-17 10:53:53 -0700

    adding hello world console message

--- unity-AR-visualization/Assets/test.cs
+++ unity-AR-visualization/Assets/test.cs
@@ -7,7 +7,7 @@ public class test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello World!");
     }
 
     // Update is called once per frame
