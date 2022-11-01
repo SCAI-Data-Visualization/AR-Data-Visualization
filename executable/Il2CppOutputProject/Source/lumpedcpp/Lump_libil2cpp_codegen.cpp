aa96765a3bc5f668ff7b9ef93ef8990d0e817226
Logan Bock <72960630+Logan-Bock@users.noreply.github.com>
2022-11-01 10:40:33 -0700

    Remove compiled version

--- executable/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_codegen.cpp
+++
@@ -1,4 +0,0 @@
-#include "il2cpp-config.h"
-#include "C:\Users\lbock\Documents\Capstone\Product\AR-Data-Visualization\executable\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-common.cpp"
-#include "C:\Users\lbock\Documents\Capstone\Product\AR-Data-Visualization\executable\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-il2cpp.cpp"
-#include "C:\Users\lbock\Documents\Capstone\Product\AR-Data-Visualization\executable\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-tiny.cpp"
--- executable/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_debugger.cpp
+++
@@ -1,2 +0,0 @@
-#include "il2cpp-config.h"
-#include "C:\Users\lbock\Documents\Capstone\Product\AR-Data-Visualization\executable\Il2CppOutputProject\IL2CPP\libil2cpp\debugger\il2cpp-api-debugger.cpp"
