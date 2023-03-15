7d17931593cd5508a60c90ab9b127aa4a95cf3bd
Brownsonny <jesuscarrera364@gmail.com>
2023-03-14 17:11:27 -0700

    Added the ability to read the JSON files

--- unity-AR-visualization/Assets/InProject/Scenes/scripts/scene_builder.cs
+++ unity-AR-visualization/Assets/InProject/Scenes/scripts/scene_builder.cs
@@ -4,13 +4,14 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Rendering;
 
 namespace jsonhelpernamespace
 {
     public class scene_builder : MonoBehaviour
     {
         public JsonHelper sceneElements;
-        public GameObject prefab; 
+        public GameObject prefab;
 
         // Start is called before the first frame update
         void Start()
@@ -18,15 +19,15 @@ namespace jsonhelpernamespace
             sceneElements = SceneBuilderController.jsonHelper;
             prefab = SceneBuilderController.gameObject;
             GameObject SpawnPoint = GameObject.Find("SpawnPoint");
-            Vector3 position = SpawnPoint.transform.position;
-            Instantiate(prefab, position, Quaternion.identity);
-        }
+            GameObject Button = Instantiate(prefab);
+            Transform ParentTransform = SpawnPoint.transform;
+            Button.transform.SetParent(ParentTransform);
+            var location = sceneElements.locations;
+            Button.transform.localPosition = new Vector3(location[0][0] * 100, location[0][1] * 100, 0);
 
-      /*  public void task_generate(JsonHelper sceneElements, GameObject prefab)
-        {
+
             JsonHelper.coordinateReader(sceneElements);
-            //make some objects
-        }*/
+        }
 
         // Update is called once per frame
         void Update()
