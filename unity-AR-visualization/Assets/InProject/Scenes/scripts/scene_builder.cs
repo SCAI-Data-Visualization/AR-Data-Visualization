1345c2da1040c7f81e195d6c897d15bab7f55158
unknown <colemanliyah@gmail.com>
2023-03-16 12:11:34 -0700

    runtime event listener

--- unity-AR-visualization/Assets/InProject/Scenes/scripts/scene_builder.cs
+++ unity-AR-visualization/Assets/InProject/Scenes/scripts/scene_builder.cs
@@ -27,7 +27,28 @@ namespace jsonhelpernamespace
             Button.transform.localPosition = new Vector3(location[0][0] * 100, location[0][1] * 100, 5);
             
             JsonHelper.coordinateReader(sceneElements);
+
+            //EventTrigger trigger = GetComponent<EventTrigger>();
+            EventTrigger.Entry entry = new EventTrigger.Entry();
+            entry.eventID = EventTriggerType.PointerClick;
+            entry.callback.AddListener((eventData) => { onClick(); });
+            trigger.triggers.Add(entry);
+            Button.GetComponent<EventTrigger>().triggers.Add(entry);
         }
+
+        void onClick()
+        {
+            Console.WriteLine("Made it with click");
+        }
+
+        //Option 1. add event listener to the prefab in the start function
+        //          have the listener point to a new function in this script
+        //          that handles the desired functions of pressing on click 
+        //Option 2. write an on click function in this script and attach it 
+        //          to the object that is made in unity (unsure if this will
+        //          work because the prefab is not premade
+
+        // Update is called once per frame
 
         // Update is called once per frame
         void Update()
