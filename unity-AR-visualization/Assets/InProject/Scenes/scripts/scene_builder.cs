9550eefeb47ce6e536d341ac191b21117a3c7030
Jesus Carrera <jesuscarrera364@gmail.com>
2023-03-22 16:39:54 -0700

    Made loading shapes more dynamic

--- unity-AR-visualization/Assets/InProject/Scenes/scripts/scene_builder.cs
+++ unity-AR-visualization/Assets/InProject/Scenes/scripts/scene_builder.cs
@@ -19,15 +19,43 @@ namespace jsonhelpernamespace
         void Start()
         {
 
-            sceneElements = SceneBuilderController.jsonHelper; 
-            prefab = SceneBuilderController.prefab;
-            GameObject SpawnPoint = GameObject.Find("SpawnPoint");
-            GameObject Button = Instantiate(prefab);
-            Transform ParentTransform = SpawnPoint.transform;
-            Button.transform.SetParent(ParentTransform);
-            var location = sceneElements.locations;
-            Button.transform.localPosition = new Vector3(location[0][0] * 100, location[0][1] * 100, 5);
-            
+            sceneElements = SceneBuilderController.jsonHelper;
+            if (sceneElements.task == "click")
+            {
+                prefab = SceneBuilderController.prefab;
+                GameObject SpawnPoint = GameObject.Find("SpawnPoint");
+                Transform ParentTransform = SpawnPoint.transform;
+                var location = sceneElements.locations;
+                List<GameObject> gameObjects = new List<GameObject>();
+                for (int i = 0; i < location.Count; i++)
+                {
+                    GameObject temp = prefab;
+                    gameObjects.Add(temp);
+                    gameObjects[i] = Instantiate(gameObjects[i]);
+                    gameObjects[i].transform.SetParent(ParentTransform);
+                    gameObjects[i].transform.localPosition = new Vector3(location[i][0] * 100, location[i][1] * 100, 5);
+                }
+            }
+            else if (sceneElements.task == "drag")
+            {
+                prefab = SceneBuilderController.prefab;
+                GameObject SpawnPoint = GameObject.Find("SpawnPoint");
+                Transform ParentTransform = SpawnPoint.transform;
+                var location = sceneElements.locations;
+                List<GameObject> gameObjects = new List<GameObject>();
+                for (int i = 0; i < location.Count; i++)
+                {
+                    GameObject temp = prefab;
+                    gameObjects.Add(temp);
+                    gameObjects[i] = Instantiate(gameObjects[i]);
+                    gameObjects[i].transform.SetParent(ParentTransform);
+                    //Button.transform.localPosition = new Vector3(location[i][0] * 100, location[i][1] * 100, 5);
+                    gameObjects[i].transform.localPosition = new Vector3(location[i][0] * 100, location[i][1] * 100, 5);
+                    Debug.Log("Im here!");
+                }
+            }
+
+
             JsonHelper.coordinateReader(sceneElements);
         }
 
